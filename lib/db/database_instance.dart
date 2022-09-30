4b6f9e77d8a8ceddb83a76d59acabcd56b097bb3
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2022-09-30 09:08:33 +0700

    update database

--- lib/db/database_instance.dart
+++ lib/db/database_instance.dart
@@ -1,6 +1,10 @@
 import 'dart:io';
 
+// Import Model
 import 'package:my_cash_book_flutter/models/transaksi_model.dart';
+import 'package:my_cash_book_flutter/models/user_model.dart';
+
+// Plugin
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
@@ -10,13 +14,21 @@ class DatabaseInstance {
   final int databaseVersion = 2;
 
   // Atribut di Model Transaksi
-  final String namaTabel = 'transaksi';
+  final String tabel_transaksi = 'transaksi';
   final String id = 'id';
   final String type = 'type';
   final String total = 'total';
   final String name = 'name';
   final String createdAt = 'created_at';
   final String updatedAt = 'updated_at';
+
+  // Atribut di Model User
+  final String tabel_user = 'user';
+  final String id_user = 'id';
+  final String username = 'username';
+  final String password = 'password';
+  final String created_at = 'created_at';
+  final String updated_at = 'updated_at';
 
   Database? _database;
   Future<Database> database() async {
@@ -34,43 +46,64 @@ class DatabaseInstance {
 
   Future _onCreate(Database db, int version) async {
     await db.execute(
-        'CREATE TABLE ${namaTabel} ($id INTEGER PRIMARY KEY, $name TEXT NULL, $type INTEGER, $total INTEGER, $createdAt TEXT NULL, $updatedAt TEXT NULL)');
+        'CREATE TABLE ${tabel_transaksi} ($id INTEGER PRIMARY KEY, $name TEXT NULL, $type INTEGER, $total INTEGER, $createdAt TEXT NULL, $updatedAt TEXT NULL)');
+    'CREATE TABLE ${tabel_user} ($id_user INTEGER PRIMARY KEY, $username TEXT NULL, $password TEXT NULL, $created_at TEXT NULL, $updated_at TEXT NULL)';
   }
 
   Future<List<TransaksiModel>> getAll() async {
-    final data = await _database!.query(namaTabel);
+    final data = await _database!.query(tabel_transaksi);
     List<TransaksiModel> result =
         data.map((e) => TransaksiModel.fromJson(e)).toList();
     return result;
   }
 
   Future<int> insert(Map<String, dynamic> row) async {
-    final query = await _database!.insert(namaTabel, row);
+    final query = await _database!.insert(tabel_transaksi, row);
     return query;
   }
 
   Future<int> totalPemasukan() async {
     final query = await _database!.rawQuery(
-        "SELECT SUM(total) as total FROM ${namaTabel} WHERE type = 1");
+        "SELECT SUM(total) as total FROM ${tabel_transaksi} WHERE type = 1");
     return int.parse(query.first['total'].toString());
   }
 
   Future<int> totalPengeluaran() async {
     final query = await _database!.rawQuery(
-        "SELECT SUM(total) as total FROM ${namaTabel} WHERE type = 2");
+        "SELECT SUM(total) as total FROM ${tabel_transaksi} WHERE type = 2");
     return int.parse(query.first['total'].toString());
   }
 
   Future<int> hapus(idTransaksi) async {
     final query = await _database!
-        .delete(namaTabel, where: '$id = ?', whereArgs: [idTransaksi]);
+        .delete(tabel_transaksi, where: '$id = ?', whereArgs: [idTransaksi]);
 
     return query;
   }
 
   Future<int> update(int idTransaksi, Map<String, dynamic> row) async {
-    final query = await _database!
-        .update(namaTabel, row, where: '$id = ?', whereArgs: [idTransaksi]);
+    final query = await _database!.update(tabel_transaksi, row,
+        where: '$id = ?', whereArgs: [idTransaksi]);
     return query;
   }
+
+  // Future<int> insertUser(Map<String, dynamic> row) async {
+  //   final query = await _database!.insert(tabel_user, row);
+  //   return query;
+  // }
+
+  // _insertUser() async {
+  //   await insertUser({
+  //     'username': 'admin',
+  //     'password': '123456',
+  //     'created_at': DateTime.now().toString(),
+  //     'updated_at': DateTime.now().toString()
+  //   });
+  // }
+
+  // Future<int> updateUser(int idUser, Map<String, dynamic> row) async {
+  //   final query = await _database!
+  //       .update(tabel_user, row, where: '$id_user = ?', whereArgs: [idUser]);
+  //   return query;
+  // }
 }
